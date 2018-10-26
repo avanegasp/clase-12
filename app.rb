55c54637cfa0d177520e33c749be7ad84cf1eafb
Angie <arco527@gmail.com>
2018-10-26 14:25:56 -0500

    Uncommit

--- app.rb
+++ app.rb
@@ -1,27 +1,27 @@
 require 'sinatra'
 
-# get '/' do
-#   erb :index
-# end
-#
-#
-# get '/dog/:name/:apellido' do
-#   p params
-#   @name = params["name"]
-#   @apellido = params["apellido"]
-#   erb :dog
-# end
+get '/' do
+  erb :index
+end
 
-# todos = []
-#
-# get '/todos/:todo' do
-#   todos.push(params["todo"])
-#   @todos = todos
-#   erb :todos
-# end
 
-#rutas GET para traer información
-#rutas POST para envíar información
+get '/dog/:name/:apellido' do
+  p params
+  @name = params["name"]
+  @apellido = params["apellido"]
+  erb :dog
+end
+
+todos = []
+
+get '/todos/:todo' do
+  todos.push(params["todo"])
+  @todos = todos
+  erb :todos
+end
+
+rutas GET para traer información
+rutas POST para envíar información
 
 todos = []
 
@@ -39,10 +39,10 @@ end
 
 
 
-# ejercicio*
+ ejercicio*
 
-#crear una ruta (/) que es la página principal (landing page)
-#una segunda ruta /cat/:name, cuya vista diga, "Mi nombre es:__"
-#una tercera ruta /dog/:name, cuya vista diga, "Mi nombre es:__"
-#una cuarta ruta /pets, cuya vista me muestre una lista de mascotas
-#que tenga las dos rutas vistas (get/postman)
+crear una ruta (/) que es la página principal (landing page)
+una segunda ruta /cat/:name, cuya vista diga, "Mi nombre es:__"
+una tercera ruta /dog/:name, cuya vista diga, "Mi nombre es:__"
+una cuarta ruta /pets, cuya vista me muestre una lista de mascotas
+que tenga las dos rutas vistas (get/postman)
